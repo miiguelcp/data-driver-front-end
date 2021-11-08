cfa365a005565f4203b91e3087dcf513aa0366f2
lbideau <leshliebzt@gmail.com>
2021-11-08 19:41:14 -0400

    Commit of perfil para editar y gestionar los datos

--- src/js/views/perfil.js
+++ src/js/views/perfil.js
@@ -3,5 +3,9 @@ import PropTypes from "prop-types";
 import { Link, useHistory } from "react-router-dom";
 
 export const perfil= ()=>{
-
+    const [firstName, setFirstname] = useState("");
+	const [lastName, setLastname] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [password, setPassword] = useState("");
 }
