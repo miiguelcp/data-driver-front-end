5fad58c755fbc749cab0c6672bbe1ec5a453cb1e
lbideau <leshliebzt@gmail.com>
2021-11-11 19:49:47 -0400

    New Commit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto p-3 text-center bg-dark">
+	<footer className="footer mt-auto p-3 text-center" style={{ backgroundColor: "#000000" }}>
 		<p className="text-light"> © Todos los derechos reservados Data Driver</p>
 	</footer>
 );
--- src/js/views/perfil.js
+++ src/js/views/perfil.js
@@ -1,13 +1,16 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Link, useHistory } from "react-router-dom";
 import Usuario from "../../img/usuario.png";
+import { AppContext } from "../store/appContext";
 
 export const Perfil = () => {
 	const [firstName, setFirstname] = useState("");
 	const [lastName, setLastname] = useState("");
 	const [email, setEmail] = useState("");
 	const [phone, setPhone] = useState("");
+	const [prueba, setPrueba] = useState("");
+	const { store, actions } = useContext(AppContext);
 
 	const history = useHistory();
 
@@ -18,26 +21,58 @@ export const Perfil = () => {
 			email: email,
 			phone_number: phone
 		};
-		const response = await fetch(URL + "/user", {
+
+		const response = await fetch(store.URL + "/user", {
 			method: "PUT",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify(data)
 		});
-		console.log(response);
-		if (response.ok) {
-			history.push("/login");
-		} else alert(response.statusText);
+
+		const getInfo = async () => {
+			try {
+				const response = await fetch(store.URL + "/user");
+				const body = await response.json();
+				console.log(body);
+				setPrueba(body);
+			} catch (error) {
+				alert("Sucedió algo inesperado");
+			}
+		};
+		const responsePrueba = await fetch(store.URL + "/user");
+		const body = await response.json();
+		console.log(body);
+		useEffect(() => {
+			getInfo();
+		}, []);
 	}
+
 	return (
 		<div className="row m-5">
 			<div className="col-4">
 				<div className="card">
 					<img src={Usuario} className="card-img-top ml-3" alt="..." />
 					<div className="card-body">
-						<p className="card-text text-centrado">First Name</p>
-						<p className="card-text text-centrado">Last Name</p>
+						<div className="container-father-perfil">
+							<p className="card-text text-centrado">First Name</p>
+							<button className="btn" id="btn-editar">
+								<i className="fas fa-pencil-alt"></i>
+							</button>
+						</div>
+						<div className="container-father-perfil">
+							<p className="card-text text-centrado">Last Name</p>
+							<button className="btn" id="btn-editar">
+								<i className="fas fa-pencil-alt"></i>
+							</button>
+						</div>
+
 						<p className="card-text text-centrado">Email</p>
-						<p className="card-text text-centrado">Phone</p>
+
+						<div className="container-father-perfil">
+							<p className="card-text text-centrado">Phone</p>
+							<button className="btn" id="btn-editar">
+								<i className="fas fa-pencil-alt"></i>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
